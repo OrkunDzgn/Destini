7962cfcc849938a3693922045333460ec90ad205
Orkun Düzgün <orkundzgn@Orkun-MacBook-Pro.local>
2017-12-27 19:29:53 +0300

    Left side of the story is done

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -30,28 +30,64 @@ class ViewController: UIViewController {
     
     
     // UI Elements linked to the storyboard
-    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
+    @IBOutlet weak var topButton: UIButton!// Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
-    // TODO Step 5: Initialise instance variables here
-    
-    
+    var questionLevel : Int = 0
+    var path : String = ""
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+        if questionLevel == 0{
+            storyTextView.text = story1
+            topButton.setTitle("\(answer1a)", for: .normal)
+            bottomButton.setTitle("\(answer1b)", for: .normal)
+        }
         
     }
 
     
-    // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
+        print(sender.tag)
+        questionLevel += 1
+       
+        if questionLevel == 1 {
+            if sender.tag == 1 {
+                storyTextView.text = story3
+                topButton.setTitle("\(answer3a)", for: .normal)
+                bottomButton.setTitle("\(answer3b)", for: .normal)
+                path = "left"
+            }
+            else if sender.tag == 2 {
+                storyTextView.text = story2
+                topButton.setTitle("\(answer2a)", for: .normal)
+                bottomButton.setTitle("\(answer2b)", for: .normal)
+                path = "right"
+            }
+        }
+        
+        
+        if questionLevel == 2 && path == "left" {
+            if sender.tag == 1 {
+                storyTextView.text = story6
+            }
+            else if sender.tag == 2 {
+                storyTextView.text = story5
+            }
+            questionLevel += 1
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+        } else if questionLevel == 2 && path == "right" {
+            if sender.tag == 1 {
+                storyTextView.text = story6
+            }
+            else if sender.tag == 2 {
+                storyTextView.text = story5
+            }
+        }
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
         
